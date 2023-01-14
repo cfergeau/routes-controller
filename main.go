6c134bc7c21944d6d86c87c7a4276288083d1ffe
Rene Raab <rene.raab@gmx.net>
2023-01-14 12:56:36 +0100

    split routes handler to separate code

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"flag"
-	"net/http"
-	"time"
-
-	v1 "github.com/openshift/api/route/v1"
+	routeshandler "github.com/code-ready/routes-controller/pkg/routes-handler"
 	routeclientset "github.com/openshift/client-go/route/clientset/versioned"
-	informers "github.com/openshift/client-go/route/informers/externalversions"
 	log "github.com/sirupsen/logrus"
-	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -37,71 +30,25 @@ func run() error {
 		log.SetLevel(log.DebugLevel)
 	}
 
+	// build config
 	config, err := clientcmd.BuildConfigFromFlags(master, kubeconfig)
 	if err != nil {
 		return err
 	}
 
-	clientset, err := routeclientset.NewForConfig(config)
-	if err != nil {
-		return err
-	}
-
+	// setup informer stop channel
 	stop := make(chan struct{})
-	return routesController(stop, clientset)
-}
+	defer close(stop)
 
-func routesController(stopCh chan struct{}, clientset *routeclientset.Clientset) error {
-	factory := informers.NewSharedInformerFactory(clientset, 5*time.Minute)
-	informer := factory.Route().V1().Routes().Informer()
-	defer close(stopCh)
-	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			route := obj.(*v1.Route)
-			log.Infof("added: %s %s", route.GetName(), route.Spec.Host)
-			if err := expose(route.Spec.Host); err != nil {
-				log.Error(err)
-			}
-		},
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			old := oldObj.(*v1.Route)
-			route := newObj.(*v1.Route)
-			if old.Spec.Host != route.Spec.Host {
-				log.Infof("updated: %s (%s -> %s)", route.GetName(), old.Spec.Host, route.Spec.Host)
-				if err := unexpose(old.Spec.Host); err != nil {
-					log.Error(err)
-				}
-				if err := expose(route.Spec.Host); err != nil {
-					log.Error(err)
-				}
-			}
-		},
-		DeleteFunc: func(obj interface{}) {
-			route := obj.(*v1.Route)
-			log.Infof("deleted: %s %s", route.GetName(), route.Spec.Host)
-			if err := unexpose(route.Spec.Host); err != nil {
-				log.Error(err)
-			}
-		},
-	})
-	informer.Run(stopCh)
+	// run routes handler
+	routesClientSet, err := routeclientset.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+	routePortHandler := routeshandler.RoutesHandler(routesClientSet)
+	go func() {
+		routePortHandler.Run(stop)
+	}()
+
 	return nil
-}
-
-func expose(host string) error {
-	bin, err := json.Marshal([]string{host})
-	if err != nil {
-		return err
-	}
-	_, err = http.Post("http://gateway/hosts/add", "application/json", bytes.NewReader(bin))
-	return err
-}
-
-func unexpose(host string) error {
-	bin, err := json.Marshal([]string{host})
-	if err != nil {
-		return err
-	}
-	_, err = http.Post("http://gateway/hosts/remove", "application/json", bytes.NewReader(bin))
-	return err
 }
