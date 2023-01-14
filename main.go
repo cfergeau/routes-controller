187b5d959a2f8ff30ec66457caa8d11e54ce1800
Rene Raab <rene.raab@gmx.net>
2023-01-14 12:56:36 +0100

    add nodeport handler

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
+	nodeporthandler "github.com/code-ready/routes-controller/pkg/node-port-handler"
 	routeshandler "github.com/code-ready/routes-controller/pkg/routes-handler"
 	routeclientset "github.com/openshift/client-go/route/clientset/versioned"
 	log "github.com/sirupsen/logrus"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -39,6 +41,16 @@ func run() error {
 	// setup informer stop channel
 	stop := make(chan struct{})
 	defer close(stop)
+
+	// run node port handler
+	nodePortClientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+	nodePortHandler := nodeporthandler.NodePortHandler(nodePortClientSet)
+	go func() {
+		nodePortHandler.Run(stop)
+	}()
 
 	// run routes handler
 	routesClientSet, err := routeclientset.NewForConfig(config)
